844e1c50cff9f34791fa23aa662142d9a7f412fc
Taha Mahmoud <taha29805@gmail.com>
2025-07-05 15:54:27 +0300

    feat: implement loading spinner functionality and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LanguageService } from './services/language.service';
-import { ChangelangComponent } from './changelang/changelang.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, ChangelangComponent],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  loading = false;
   constructor(private languageService: LanguageService) {
     this.languageService.init();
+  }
+
+  ngOnInit(): void {
+    this.loadingSpinner();
+  }
+  loadingSpinner() {
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+    }, 3000);
   }
 }

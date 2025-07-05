89c85aad62ab2b58ab656713c25886c0beaf2338
Taha Mahmoud <taha29805@gmail.com>
2025-07-05 13:13:12 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LanguageService } from './services/language.service';
+import { ChangelangComponent } from './changelang/changelang.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, ChangelangComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
+  constructor(private languageService: LanguageService) {
+    this.languageService.init();
+  }
 }
